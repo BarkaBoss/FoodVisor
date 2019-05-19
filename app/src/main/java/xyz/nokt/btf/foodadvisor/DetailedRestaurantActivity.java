aba06bb00e44c9623a536c5453ce20a914f225f3
Barka Fori <bossmajor.biii@gmail.com>
2019-05-19 18:37:52 +0100

    get user location

--- app/src/main/java/xyz/nokt/btf/foodadvisor/DetailedRestaurantActivity.java
+++ app/src/main/java/xyz/nokt/btf/foodadvisor/DetailedRestaurantActivity.java
@@ -2,11 +2,15 @@ package xyz.nokt.btf.foodadvisor;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -15,14 +19,17 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.picasso.Picasso;
 
 public class DetailedRestaurantActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     private GoogleMap mMap;
 
     TextView restName, restPhone, restAddress;
+    ImageView banner;
 
     String address;
     @Override
@@ -33,10 +40,15 @@ public class DetailedRestaurantActivity extends FragmentActivity implements OnMa
         restName = findViewById(R.id.restName);
         restPhone = findViewById(R.id.restPhone);
         restAddress = findViewById(R.id.restAddress);
+        banner = findViewById(R.id.bBanner);
+
+        Picasso.get().load(getIntent()
+        .getStringExtra("imageBanner")).into(banner);
 
         restPhone.setText(getIntent().getStringExtra("phone"));
         restName.setText(getIntent().getStringExtra("restName"));
         restAddress.setText(getIntent().getStringExtra("restAddress"));
+
 
         address = getIntent().getStringExtra("restAddress");
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -51,11 +63,21 @@ public class DetailedRestaurantActivity extends FragmentActivity implements OnMa
         mMap = googleMap;
         mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
 
+
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
         }
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
+
+            mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                @Override
+                public void onMyLocationChange(Location location) {
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLatitude()))
+                            .title("You"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLatitude())));
+                }
+            });
         }
     }
 
--- app/src/main/java/xyz/nokt/btf/foodadvisor/RestaurantListAdapter.java
+++ app/src/main/java/xyz/nokt/btf/foodadvisor/RestaurantListAdapter.java
@@ -3,6 +3,8 @@ package xyz.nokt.btf.foodadvisor;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,12 +13,15 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +30,7 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
     private Activity activity;
     private List<RestaurantObj> restLists;
     private Context context;
+    Bitmap mBitmap;
 
     FirebaseDatabase fireDatabase = FirebaseDatabase.getInstance();
     FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
@@ -46,7 +52,7 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
         final RestaurantObj restaurantObj = restLists.get(position);
 
         holder.tvRestPhone.setText(restaurantObj.getRest_phone());
@@ -55,13 +61,37 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
 
         Picasso.get().load(restaurantObj.rest_imgUrl).into(holder.restBanner);
 
+        /*Picasso.get().load(restaurantObj.rest_imgUrl)
+                .into(new Target() {
+                    @Override
+                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                        holder.restBanner.setImageBitmap(bitmap);
+                        mBitmap = bitmap;
+                    }
+
+                    @Override
+                    public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+                        Toast.makeText(context, "Network too slow",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                    }
+                });*/
+
         holder.rel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /*ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                mBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();*/
+
                 Intent goMapsIntent = new Intent(activity, DetailedRestaurantActivity.class);
                 goMapsIntent.putExtra("phone", restaurantObj.getRest_phone());
                 goMapsIntent.putExtra("restName", restaurantObj.getRest_name());
                 goMapsIntent.putExtra("restAddress", restaurantObj.getRest_address());
+                goMapsIntent.putExtra("imageBanner", restaurantObj.getRest_imgUrl());
 
                 context.startActivity(goMapsIntent);
             }

c3891567433897c238a194d58ea926e089b0dc74
Barka Fori <bossmajor.biii@gmail.com>
2019-05-22 23:43:02 +0100

    Getting Location from address

--- app/src/main/java/xyz/nokt/btf/foodadvisor/DetailedRestaurantActivity.java
+++ app/src/main/java/xyz/nokt/btf/foodadvisor/DetailedRestaurantActivity.java
@@ -1,10 +1,13 @@
 package xyz.nokt.btf.foodadvisor;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -15,6 +18,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -26,6 +30,9 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.List;
 
 public class DetailedRestaurantActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
@@ -99,6 +106,28 @@ public class DetailedRestaurantActivity extends FragmentActivity implements OnMa
         }
     }
 
+    private LatLng getLatLongFromAdd(Context context, String strAddress)
+    {
+        Geocoder gCoder = new Geocoder(context);
+        List<Address> addresses;
+        LatLng addLL = null;
+
+        try{
+            addresses = gCoder.getFromLocationName(strAddress, 1);
+            if(addresses == null)
+            {
+                Toast.makeText(context, "Location not found", Toast.LENGTH_LONG).show();
+            }
+
+            Address loc = addresses.get(0);
+            addLL = new LatLng(loc.getLatitude(), loc.getLongitude());
+        }catch (IOException ioEx)
+        {
+            ioEx.printStackTrace();
+        }
+
+        return addLL;
+    }
     @Override
     public void onConnected(@Nullable Bundle bundle) {
 

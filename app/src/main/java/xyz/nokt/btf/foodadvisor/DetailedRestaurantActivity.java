b9de45c926e664e8a41502256f4dc8056989a60f
Barka Fori <bossmajor.biii@gmail.com>
2019-05-23 13:50:26 +0100

    Add routes to restaurants

--- app/src/main/java/xyz/nokt/btf/foodadvisor/DetailedRestaurantActivity.java
+++ app/src/main/java/xyz/nokt/btf/foodadvisor/DetailedRestaurantActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -15,6 +16,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,12 +28,24 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DetailedRestaurantActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
@@ -40,6 +54,7 @@ public class DetailedRestaurantActivity extends FragmentActivity implements OnMa
 
     TextView restName, restPhone, restAddress, restCallin;
     ImageView banner;
+    LatLng userLoc, restLoc;
 
     String address;
     @Override
@@ -86,8 +101,7 @@ public class DetailedRestaurantActivity extends FragmentActivity implements OnMa
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
-
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
@@ -100,7 +114,77 @@ public class DetailedRestaurantActivity extends FragmentActivity implements OnMa
                 public void onMyLocationChange(Location location) {
                     mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLatitude()))
                             .title("You"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLatitude())));
+                    userLoc = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(userLoc));
+
+                    //Plot Route Code
+
+                    List<LatLng> path = new ArrayList();
+                    restLoc = getLatLongFromAdd(getBaseContext(), address);
+
+                    //userLoc = new LatLng(8.9576992, 7.7007401);
+
+
+                    //SphericalUtil.computeDistanceBetween(passLoc, path.get(0));
+                    BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher);
+
+                    GeoApiContext context = new GeoApiContext.Builder()
+                            .apiKey("AIzaSyAK_TyRo84WCqj5CI14wisdSTya4Vdxehk")
+                            .build();
+
+                    mMap.addMarker(new MarkerOptions().position(userLoc).title("You are here"));
+                    DirectionsApiRequest req = DirectionsApi.getDirections(context, Double.toString(userLoc.latitude)+","+Double.toString(userLoc.longitude), Double.toString(restLoc.latitude)+","+Double.toString(restLoc.longitude));
+                    //DirectionsApiRequest req = DirectionsApi.getDirections(context, "8.9576992, 7.7007401", "6.595680,3.337030");
+                    try {
+                        DirectionsResult res = req.await();
+
+                        //Loop through legs and steps to get encoded polylines of each step
+                        if (res.routes != null && res.routes.length > 0) {
+                            DirectionsRoute route = res.routes[0];
+
+                            if (route.legs !=null) {
+                                for(int i=0; i<route.legs.length; i++) {
+                                    DirectionsLeg leg = route.legs[i];
+                                    if (leg.steps != null) {
+                                        for (int j=0; j<leg.steps.length;j++){
+                                            DirectionsStep step = leg.steps[j];
+                                            if (step.steps != null && step.steps.length >0) {
+                                                for (int k=0; k<step.steps.length;k++){
+                                                    DirectionsStep step1 = step.steps[k];
+                                                    EncodedPolyline points1 = step1.polyline;
+                                                    if (points1 != null) {
+                                                        //Decode polyline and add points to list of route coordinates
+                                                        List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                                        for (com.google.maps.model.LatLng coord1 : coords1) {
+                                                            path.add(new LatLng(coord1.lat, coord1.lng));
+                                                        }
+                                                    }
+                                                }
+                                            } else {
+                                                EncodedPolyline points = step.polyline;
+                                                if (points != null) {
+                                                    //Decode polyline and add points to list of route coordinates
+                                                    List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                                    for (com.google.maps.model.LatLng coord : coords) {
+                                                        path.add(new LatLng(coord.lat, coord.lng));
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    } catch(Exception ex) {
+                        Log.e("Localised", ex.getLocalizedMessage());
+                    }
+
+                    //Draw the polyline
+                    if (path.size() > 0) {
+                        PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
+                        mMap.addPolyline(opts);
+                    }
+                    //End Plot Route
                 }
             });
         }

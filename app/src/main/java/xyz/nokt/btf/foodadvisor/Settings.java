2d830054f38631ad801ee2dcaeffa575cd0a214d
Barka Fori <bossmajor.biii@gmail.com>
2019-05-25 21:45:12 +0100

    Added Settings

--- app/src/main/java/xyz/nokt/btf/foodadvisor/Settings.java
+++ app/src/main/java/xyz/nokt/btf/foodadvisor/Settings.java
@@ -7,9 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class Settings extends Fragment {
+
+    FirebaseAuth fbAuth = FirebaseAuth.getInstance();
+    FirebaseDatabase fbDB = FirebaseDatabase.getInstance();
+    DatabaseReference dbRef = fbDB.getReference("User");
+
+    EditText edDiet;
+    Button btnChangeDiet;
 
     private OnFragmentInteractionListener mListener;
 
@@ -29,7 +42,20 @@ public class Settings extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        edDiet = rootView.findViewById(R.id.edDietaryChoice);
+        btnChangeDiet = rootView.findViewById(R.id.btnChangeDiet);
+
+
+        btnChangeDiet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbRef.child(fbAuth.getCurrentUser().getUid().toString()).child("cusDiet").setValue(edDiet.getText().toString());
+            }
+        });
+
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
